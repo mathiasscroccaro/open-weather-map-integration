2fe887acad58511045a912df0339a0752657e5fc
Mathias Scroccaro Costa <mathias.scroccaro@gmail.com>
2022-04-17 07:57:01 -0300

    Implemented all resources endpoints

--- weather_api/weather_api/resources/__init__.py
+++ weather_api/weather_api/resources/__init__.py
@@ -1,9 +1,10 @@
 from flask_restful import Api
 
 from weather_api import app
-from weather_api.resources.temperature import CityName
+from weather_api.resources.temperature import CityName, LastNWeathers
 
 api = Api(app)
 
-api.add_resource(CityName, '/temperature')
+api.add_resource(CityName, '/temperature/<string:city_name>')
+api.add_resource(LastNWeathers, '/temperature')
 
--- weather_api/weather_api/resources/temperature.py
+++ weather_api/weather_api/resources/temperature.py
@@ -1,6 +1,23 @@
-from flask_restful import Resource
+from weather_api import weather_data
+from flask_restful import Resource, reqparse
 
 
 class CityName(Resource):
+    def get(self, city_name):
+        return weather_data.get_from_city(city_name)
+
+
+class LastNWeathers(Resource):
     def get(self):
-        return "hello"
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            'max',
+            default=5,
+            location=['args', 'form'],
+            type=int
+        )
+        args = parser.parse_args()
+        n = args['max']
+        return weather_data.get_last_n_weathers(n)
+
+
