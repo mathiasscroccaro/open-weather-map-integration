ebb1664f817928e6e0ab520dbdef8ccdd2bfc1a7
Mathias Scroccaro Costa <mathias.scroccaro@gmail.com>
2022-04-17 06:57:25 -0300

    Implemented API configuration through environment variables and added WeatherData cache to the API

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # DevGrid - Weather Wrapper API
 
-http://api.mathias.dev.br:5000/temperature
+# Examples
+
+http://api.mathias.dev.br:5000/temperature/london
+
+http://api.mathias.dev.br:5000/temperature/campinas
+
+http://api.mathias.dev.br:5000/temperature?max=2
+
+# How to configure
+
+There are 3 environment variables in the docker-compose.yml file that must be set:
+
+- CACHE_TTL: Time To Live in seconds for the cached requests
+- DEFAULT_MAX_NUMBER: Max number of cached messages stored
+- API_KEY: OpenWeather api key
 
 # How to run
 
--- weather_api/weather_api/__init__.py
+++ weather_api/weather_api/__init__.py
@@ -1,5 +1,37 @@
+import os
+
 from flask import Flask
+from weather_api.cache import WeatherData
+
+
+def get_environment_variable(variable):
+    try:
+        return os.environ[variable]
+    except KeyError:
+        if variable == 'CACHE_TTL':
+            return 5*60
+        elif variable == 'DEFAULT_MAX_NUMBER':
+            return 5
+        else:
+            raise NotImplemented(f'Default variable {variable} not implemented')
+
+
+def get_environment_variables():
+    return {
+        'CACHE_TTL': get_environment_variable('CACHE_TTL'),
+        'DEFAULT_MAX_NUMBER': get_environment_variable(
+            'DEFAULT_MAX_NUMBER'),
+        'API_KEY': get_environment_variable('API_KEY')
+    }
+
 
 app = Flask(__name__)
+
+env_variables = get_environment_variables()
+weather_data = WeatherData(
+    cache_ttl=int(env_variables['CACHE_TTL']),
+    default_max_number=int(env_variables['DEFAULT_MAX_NUMBER']),
+    api_key=env_variables['API_KEY']
+)
 
 from weather_api import resources
